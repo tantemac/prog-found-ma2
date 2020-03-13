ad35372a88c78bee8605e3907a5a3ffefd7b786f
tantemac <58442619+tantemac@users.noreply.github.com>
2020-03-13 13:24:26 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+// question 1
+var petArray = ["cat", "cow", "dog"];
+
+// question 2
+console.log(petArray[1]);
+
+// question 3
+petArray.push("sheep");
+
+// question 4
+console.log(petArray.length);
+
+// question 5
+var catObject = {
+	name: "Missi",
+	colour: "grey",
+	age: 14
+};
+
+// question 6
+console.log(catObject.age);
+
+// question 7
+var catArray = [
+	{
+		name: "Missi",
+		colour: "grey",
+		age: 14
+	}
+];
+
+// question 8
+for (var i = 0; i < catArray.length; i++) {
+	console.log(catArray[i]);
+}
+
+// question 9
+function logToConsole(myPet) {
+	console.log(myPet);
+}
+
+// question 10
+logToConsole("donkey");
